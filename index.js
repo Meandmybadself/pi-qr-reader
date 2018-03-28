606aac6cba67314410652a11fc778c04624367c1
Jeffery Bennett <me@meandmybadself.com>
2018-03-28 06:52:34 +0000

    Progress, but the camera's not pausing.

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const cam = require('raspberry-pi-camera-native')
 const fs = require('fs')
 const zbar = require('node-zbarimg')
+const _ = require('lodash')
+
+let i = 0
 
 cam.on('frame', frame => {
-    const path = './images/image.jpg'
-    var stream = fs.createWriteStream(path)
-    stream.once('open', (fd) => {
-        stream.write(frame)
-        stream.end()
-        // Now, tell zbar to process.
-        zbar(path, (err, code) => {
-            console.log('+', err, code)
-        })
+    
+        // Pause the camera recording to process the QR code.
+        cam.pause(() => {
+
+            const path = `./images/image.jpg`
+            var stream = fs.createWriteStream(path)
+            stream.once('open', (fd) => {
+                stream.write(frame)
+                stream.end()
+            
+                // Tell zbar to process.
+                zbar(path, (err, code) => {
+                    if (!err) {
+                        console.log('code', code)
+                    } else {
+                        process.stdout.write('.')
+                    }
+                    // cam.resume()
+                })
+            })
     })
 })
 
 cam.start({
-    fps:5
+    fps: 10,
+    quality: 75,
+    width: 840,
+    height: 616,
 })
